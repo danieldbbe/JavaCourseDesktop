53fbc5c37eccf7286fd084967b634e325922b482
Daniel Bergqvist <danieldbbe@gmail.com>
2016-03-01 14:54:28 +0100

    something not right

--- test/Main.java
+++ test/Main.java
@@ -5,7 +5,9 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello test!");
 		System.out.println("asdasd");
-		String hej = Answer[0];
+		
+		String hej = Ansr[0];
+		
 	}
 
 }
